c10da37a4692babd31e65170003239b15df2050d
darkness8129 <yaroslaw460@gmail.com>
2020-09-22 21:46:08 +0300

    my commit 15 changes

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+//add func
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//sub func
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//mul func
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
+}
+
+//div func
+int Calculator::Div (double a, double b)
+{
+    return a/b;
 }
